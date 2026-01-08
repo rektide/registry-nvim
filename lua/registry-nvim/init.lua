1a3950d151afcbc9ec00452789b9b83aa28bcc8d
rektide <rektide+scm@voodoowarez.com>
2026-01-08 18:36:43 -0500

    Implement init.lua plugin initialization

--- lua/registry-nvim/init.lua
+++ lua/registry-nvim/init.lua
@@ -1,9 +1,38 @@
 local M = {}
 
+local monitor = require("registry-nvim.monitor")
+local cleanup = require("registry-nvim.cleanup")
+local config = require("registry-nvim.config")
+
 function M.setup(opts)
   opts = opts or {}
 
+  if not config.has_neoconf() then
+    vim.notify("registry-nvim requires neoconf.nvim", vim.log.levels.ERROR)
+    return M
+  end
+
+  monitor.init()
+
+  vim.api.nvim_create_user_command("RegistryCleanup", function()
+    cleanup.cleanup_registry()
+  end, {
+    desc = "Clean up stale listen sockets from the registry",
+  })
+
   return M
+end
+
+function M.start_monitor()
+  monitor.start()
+end
+
+function M.stop_monitor()
+  monitor.stop()
+end
+
+function M.cleanup()
+  return cleanup.cleanup_registry()
 end
 
 return M
