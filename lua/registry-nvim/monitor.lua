6e3fc8ff7511e65b4ac04f48bece376e9919e41e
rektide <rektide+scm@voodoowarez.com>
2026-01-08 18:35:50 -0500

    Implement monitor.lua for listen server discovery

--- lua/registry-nvim/monitor.lua
+++ lua/registry-nvim/monitor.lua
@@ -1,12 +1,49 @@
 local M = {}
 
+local registry = require("registry-nvim.registry")
+local config = require("registry-nvim.config")
+
+local augroup = vim.api.nvim_create_augroup("RegistryNvimMonitor", { clear = true })
+
+function M.get_current_server_socket()
+  return vim.v.servername
+end
+
+function M.register_current_server()
+  local socket = M.get_current_server_socket()
+  if socket and socket ~= "" then
+    if not registry.contains(socket) then
+      registry.add(socket)
+      vim.notify("Registered listen server: " .. socket, vim.log.levels.INFO)
+    end
+  end
+end
+
 function M.init()
+  M.register_current_server()
 end
 
 function M.start()
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = augroup,
+    callback = function()
+      vim.schedule(function()
+        M.register_current_server()
+      end)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("User", {
+    group = augroup,
+    pattern = "RemoteConnected",
+    callback = function()
+      M.register_current_server()
+    end,
+  })
 end
 
 function M.stop()
+  vim.api.nvim_clear_autocmds({ group = augroup })
 end
 
 return M
